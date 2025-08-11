c7fe96b6c7ea0e5ff8e0184e1ac6bed6d111285b
ZeroMey <87927024+ZeroMey@users.noreply.github.com>
2025-08-11 21:26:45 +0800

    Update PPGSensor.cpp

--- src/libraries/PPGSensor.cpp
+++ src/libraries/PPGSensor.cpp
@@ -1,6 +1,15 @@
 #include "DFRobot_BloodOxygen_S.h"
 
+#define I2C_ADDRESS 0x57
+DFRobot_BloodOxygen_S_I2C MAX30102(&Wire, I2C_ADDRESS);
+
 static void initPPGSensor() {
+
+  while (false == MAX30102.begin())
+  {
+    Serial.println("init fail!");
+    delay(1000);
+  }
   // Initialize MAX30102 heart rate & SpO2 sensor
   // max30102.begin(); // Use your library’s init
 }
