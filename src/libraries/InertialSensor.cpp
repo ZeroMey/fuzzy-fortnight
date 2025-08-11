27913f1ac8fd4621a788e216bcfc943959557da0
ZeroMey <87927024+ZeroMey@users.noreply.github.com>
2025-08-11 13:45:30 +0800

    Create InertialSensor.cpp

---
+++ src/libraries/InertialSensor.cpp
@@ -0,0 +1,7 @@
+void initInertialSensor() {
+  // Initialize QMI8658 accelerometer
+  // if (!qmi8658.begin()) {
+  //   Serial.println("QMI8658 initialization failed!");
+  //   while (1) { delay(10); }
+  // }
+}
